519a2465d304c586828313b43be63b1ab3d11401
Tovmas Petrosyan <tompetrosyan30@gmail.com>
2023-11-10 22:20:28 +0400

    Create fakePromise class

---
+++ promise.js
@@ -0,0 +1,57 @@
+class fakePromise {
+    constructor(executionFunction) {
+        this.state = 'pending';
+        this.value = undefined;
+        this.reason = undefined;
+        this.onFulfilled = [];
+        this.onRejected = [];
+
+        const resolve = (value) => {
+            if (this.state === 'pending') {
+                this.state = 'fulfilled';
+                this.value = value;
+                this.onFulfilled.forEach(callback => callback(this.value));
+            }
+        };
+        const reject = (reason) => {
+            if (this.state === 'pending') {
+                this.state = 'rejected';
+                this.reason = reason;
+                this.onRejected.forEach(callback => callback(this.reason));
+            }
+        };
+        try {
+            executionFunction(resolve, reject)
+        } catch (error) {
+                reject(error);
+        }
+
+    }
+
+    then(Fulfilled,Rejected){
+        switch (this.state){
+            case 'fulfilled':
+            onFulfilled(this.value);
+            break;
+            case 'rejected':
+            onRejected(this.reason);
+            break;
+            default:
+                this.onFulfilled.push(Fulfilled);
+                this.onRejected.push(Rejected);
+        }
+        return this;
+    }
+    catch(onRejected) {
+        if (this.state === 'rejected') {
+          onRejected(this.reason);
+        } else {
+          this.onRejected.push(onRejected);
+        }
+    
+        return this; 
+      }
+
+}
+
+
